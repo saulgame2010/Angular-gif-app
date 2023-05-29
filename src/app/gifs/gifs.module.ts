2c892e915c939cf87242f044c5d70b00834c2a33
saulgame2010 <saulgame2010@gmail.com>
2023-05-29 14:45:23 -0600

    Aplicación de gifs terminada

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifCardComponent } from './components/gif-card/gif-card.component';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
+    CardListComponent,
+    GifCardComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
